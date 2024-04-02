eeb8be0d00c753cb7d61558d0bc8a3d769696f42
Julian Lima <julianlimalbs@gmail.com>
2024-04-02 11:01:08 -0300

    feat: add error handling to checkBalance function

--- check-balance.ts
+++ check-balance.ts
@@ -1,17 +1,20 @@
-import { Connection, LAMPORTS_PER_SOL } from '@solana/web3.js';
+import { Connection, LAMPORTS_PER_SOL, PublicKey } from '@solana/web3.js';
 import { generateKeyPair } from './generate-keypair';
 
 export async function checkBalance() {
-  const keypair = generateKeyPair();
-  const publicKey = keypair.publicKey;
+  try {
+    const publicKey = new PublicKey('DPxe3juWL2QNY2jBfvqQ9KzAqpTPTkFWVeeKxjf1EDMW')
 
-  const connection = new Connection("https://api.devnet.solana.com", "confirmed");
-  const balanceInLamports = await connection.getBalance(publicKey)
-  const balanceInSOL = balanceInLamports / LAMPORTS_PER_SOL;
+    const connection = new Connection("https://api.devnet.solana.com", "confirmed");
+    const balanceInLamports = await connection.getBalance(publicKey)
+    const balanceInSOL = balanceInLamports / LAMPORTS_PER_SOL;
 
-  console.log(
-    `💰 Finished! The balance for the wallet at address ${publicKey} is ${balanceInSOL}!`
-  );
+    console.log(
+      `💰 Finished! The balance for the wallet at address ${publicKey} is ${balanceInSOL}!`
+    );
+  } catch (error) {
+    console.error(error)
+  }
 }
 
 checkBalance()
