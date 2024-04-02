07243eddd523e798e924bd045dd476ca0c987ef5
Julian Lima <julianlimalbs@gmail.com>
2024-04-02 10:49:55 -0300

    Implement check balance

--- generate-keypair.ts
+++ generate-keypair.ts
@@ -1,7 +1,12 @@
 import { Keypair } from '@solana/web3.js'
 
-const keypair = Keypair.generate();
+export function generateKeyPair(): Keypair {
+  const keypair = Keypair.generate();
 
-console.log(`The public key is: `, keypair.publicKey.toBase58());
-console.log(`The secret key is: `, keypair.secretKey);
-console.log(`✅ Finished!`);
+  console.log(`The public key is: `, keypair.publicKey.toBase58());
+  console.log(`The secret key is: `, keypair.secretKey);
+  console.log(`✅ Finished!`);
+
+  return keypair
+}
+
