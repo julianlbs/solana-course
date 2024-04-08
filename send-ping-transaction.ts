080794e4fd127f1b20890485645aed0b02cfc519
Julian Lima <julianlimalbs@gmail.com>
2024-04-08 17:13:55 -0300

    add send-ping-transaction.ts script

---
+++ send-ping-transaction.ts
@@ -0,0 +1,33 @@
+import "dotenv/config";
+import * as web3 from "@solana/web3.js";
+import { airdropIfRequired, getKeypairFromEnvironment } from '@solana-developers/helpers';
+
+const payer = getKeypairFromEnvironment('SECRET_KEY');
+const connection = new web3.Connection(web3.clusterApiUrl("devnet"));
+
+const newBalance = await airdropIfRequired(
+  connection,
+  payer.publicKey,
+  1 * web3.LAMPORTS_PER_SOL,
+  0.5 * web3.LAMPORTS_PER_SOL
+);
+
+const PING_PROGRAM_ADDRESS = new web3.PublicKey('ChT1B39WKLS8qUrkLvFDXMhEJ4F1XZzwUNHUt4AU9aVa');
+const PING_PROGRAM_DATA_ADDRESS = new web3.PublicKey('Ah9K7dQ8EHaZqcAsgBW8w37yN2eAy3koFmUn4x3CJtod');
+
+const transaction = new web3.Transaction();
+const instruction = new web3.TransactionInstruction({
+  keys: [
+    {
+      pubkey: PING_PROGRAM_DATA_ADDRESS,
+      isSigner: false,
+      isWritable: true
+    }
+  ],
+  programId: PING_PROGRAM_ADDRESS,
+});
+
+transaction.add(instruction);
+
+const signature = await web3.sendAndConfirmTransaction(connection, transaction, [payer]);
+console.log(`✅ Transaction completed! Signature is ${signature}`);
